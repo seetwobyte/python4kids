0160c359272a465477a7816661139a94670ef74e
seetwobyte <successscotty@gmail.com>
2018-05-21 23:36:11 -0400

    initial commit

---
+++ 6-conditionals.py
@@ -0,0 +1,26 @@
+
+
+age = 21
+
+if age > 16:
+    print("you are old enough to drive")
+else:
+    print("you cannot drive")
+
+
+    if age  >= 21:
+        print(" you can drive my tesla")
+    elif age >= 16:
+
+        print(" you are old enough to drive")
+    else:
+        print("you should not drive")
+
+# comnbine conditions with logical operators #
+# the logical operators are AND and OR #
+
+if ((age >= 1) and (age <= 18)):
+    print(" you get a birthday")
+
+elif ((age == 21)  or (age >= 65)):
+    print("you dont ge a birthday")
