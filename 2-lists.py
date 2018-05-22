2dce9c187df4c302c5288acada8d5b9c98275e58
seetwobyte <successscotty@gmail.com>
2018-05-21 22:43:26 -0400

    initial commit

--- 2-lists.py
+++ 2-lists.py
@@ -1,12 +1,38 @@
 # list use [] like a Box
 
-import _random
-import  sys
-import OS
-
-
 
 grocery_list = ["juice", "tomatoes", "potatoes", "bananas"]
 
 print("first Item", grocery_list[0])
 
+
+grocery_list[0] = "green juice"
+
+print("first item",grocery_list[0])
+
+# pick items in the list #
+
+
+# combine the list  #
+
+
+other_events = ["cut lawn ", "pick up kids", "car wash"]
+
+to_do_list = [other_events, grocery_list]
+
+print(to_do_list)
+
+
+print((to_do_list[1][2]))
+
+# add to the list s=using the .append comand #
+
+
+grocery_list.append("lemons")
+print(to_do_list)
+
+grocery_list.insert(1, "pickle")
+print(to_do_list)
+
+
+grocery_list.sort()
