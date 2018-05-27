de3311c90b57afa0010302d795a4a7ace378285b
seetwobyte <successscotty@gmail.com>
2018-05-27 19:00:27 -0400

    change comments

---
+++ 4_loop.py
@@ -0,0 +1,13 @@
+##### for loop  ##########
+# allow you to execute an operation a number of times
+# I want to do something ten times
+
+for x in range(0, 10):
+    print(x,' ', end='')
+
+print('\n')
+
+grocery_list = ['juice', 'tomatoes', 'potatoes', 'bananas']
+
+    for y in grocery_list:
+        print(y)
