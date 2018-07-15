88818c69918693f78feee94bfe6906737e1b4b4f
seetwobyte <successscotty@gmail.com>
2018-07-15 16:42:26 -0400

    adding now file

---
+++ Aiden-1.py
@@ -0,0 +1,15 @@
+#list
+
+friends = ["dave", "Mark", "Steve", "Barbara"]
+print(friends)
+
+family = ["Makayla", "Dad", "Aiden", "Mom"]
+print(family)
+
+people = friends + family
+
+print(family) + (friends)
+
+print(people)
+
+
